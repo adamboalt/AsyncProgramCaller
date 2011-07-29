424220bd936e51ef6a3e727d3c939b77b4cc7a0c
Jonathan Best <jonathan.best@student.kit.edu>
2011-07-29 02:52:04 +0200

    initial commit

---
+++ AsyncProgramCaller/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace AsyncProgramCaller
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo info = new ProcessStartInfo()
+            {
+                FileName = "Sleeper.exe",
+                Arguments = "5000",
+                WorkingDirectory = Environment.CurrentDirectory
+            };
+            var p = Process.Start(info);
+            var started = DateTime.Now;
+            p.WaitForExit();
+            Console.WriteLine("Exit code was {0}", p.ExitCode);
+            Console.WriteLine("I waited for {0}", DateTime.Now - started);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Sleeper/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sleeper
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int duration;
+            
+            if (args.Length > 0)
+            {
+                if (!Int32.TryParse(args[0], out duration))
+                {
+                    Console.WriteLine("Could not read integer.");
+                    Help();
+                    return -2;
+                }
+                else
+                {
+                    Console.WriteLine("Good night…");
+                    System.Threading.Thread.Sleep(duration);
+                    Console.WriteLine("Hello!");
+                    return 0;
+                }
+            }
+            else
+            {
+                Help();
+                return -1;
+            }
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine("Sleeper.exe does nothing for an amount of time\nUsage: Sleeper.exe <duration>\n\t<duration>\t duration of sleep in milliseconds");
+        }
+    }
+}
