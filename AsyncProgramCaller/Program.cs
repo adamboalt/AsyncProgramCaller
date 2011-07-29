636bd99b96ab954e9924a74f2736fe160ae2ec4c
Jonathan Best <jonathan.best@student.kit.edu>
2011-07-29 14:22:04 +0200

    Programm mit asynchronem warten

--- AsyncProgramCaller/Program.cs
+++ AsyncProgramCaller/Program.cs
@@ -3,25 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncProgramCaller
 {
     class Program
     {
+        // Flag: Prozess beendet
+        static bool done = false;
+
         static void Main(string[] args)
         {
+            // Prozess vorbereiten
             ProcessStartInfo info = new ProcessStartInfo()
             {
-                FileName = "Sleeper.exe",
-                Arguments = "5000",
+                FileName = "Sleeper.exe", // Ein Programm, dass nichts tut
+                Arguments = "3000", // Laufzeit: 3 Sekunden
                 WorkingDirectory = Environment.CurrentDirectory
             };
-            var p = Process.Start(info);
-            var started = DateTime.Now;
-            p.WaitForExit();
+            var p = new Process();
+            p.StartInfo = info;
+            // Signal-Hilfsfunktion
+            p.Exited += new EventHandler(p_Exited);
+            p.EnableRaisingEvents = true;
+
+            // Prozess Start
+            var started = DateTime.Now;            
+            p.Start();
+
+            // beginn (warten)
+            int i = 0;
+            while (!done)
+            {
+                Console.WriteLine(++i);
+                p.WaitForExit(1000);
+            }
+            // ende (warten)
+
+            // Prozess Ende
             Console.WriteLine("Exit code was {0}", p.ExitCode);
-            Console.WriteLine("I waited for {0}", DateTime.Now - started);
+            Console.WriteLine("I waited for {0}", p.ExitTime - started);
             Console.ReadKey();
+        }
+
+        static void p_Exited(object sender, EventArgs e)
+        {
+            done = true;
         }
     }
 }
